8b800dd260aa82d79a83e9bc902ad6536677d4f9
Gennadiy <renardes@gmail.com>
2022-09-30 17:56:33 +0500

    08.21: правки удобства чтения кода

--- 08.21/2.py
+++ 08.21/2.py
@@ -1,37 +1,45 @@
 import datetime
 
+
 class TimeZone:
+    # КОММЕНТАРИЙ: специальные методы мы размещаем наверху, пока нет весомых причин сделать иначе
+    def __init__(self, offset: int, tz_name: str):
+        # атрибут экземпляра для задания названия часового пояса
+        self.tz_name = tz_name
+        self.offset = offset
+        self.daypart = self.daytime()
+
+    def __str__(self):
+        return f'{self.daypart}\n{self.time()} {self.tz_name}'
+
+    def daytime(self):
+        """ метод для определения периода суток """
+        now = str(self.time()).split()
+        now = now[1].split(':')
+        hours = int(now[0])
+        # ИСПОЛЬЗОВАТЬ: как это вы пропустили, что Python позволяет записывать неравенства математически?
+        if 5 <= hours < 12:
+            return 'Сейчас утро'
+        elif 12 <= hours < 16:
+            return 'Сейчас день'
+        elif 16 <= hours < 24:
+            return 'Сейчас вечер'
+        else:
+            return 'Сейчас ночь'
+
     def time(self):
         """ метод для вычисления времени в заданном часовом поясе """
         offset = datetime.timedelta(hours=self.offset)
-        tz = datetime.timezone(offset, name=self.nemetimezone)
+        tz = datetime.timezone(offset, name=self.tz_name)
         dt = datetime.datetime.now(tz=tz)
         tz.utcoffset(dt)
         return dt
 
-    def daytime(self):
-        """ метод для определения времени суток """
-        timenow = str(self.time()).split()
-        timenow = timenow[1].split(':')
-        hours = int(timenow[0])
-        if hours >= 5 and hours < 12:
-            return 'Сейчас утро'
-        elif hours >= 12 and hours < 16:
-            return 'Сейчас день'
-        elif hours >= 16 and hours <24:
-            return 'Сейчас вечер'
-        else: return 'Сейчас ночь'
 
-    def __init__(self, offset: int, nemetimezone: str):
-        # свойство экземпляра для задания названия часового пояса
-        self.nemetimezone = nemetimezone
-        self.offset = offset
-        self.timenow = self.daytime()
+tz1 = TimeZone(5, 'utc')
+print(tz1)
 
-    def __str__(self):
-        return f'{self.timenow}\n{self.time()} {self.nemetimezone}'
-tz = TimeZone(4, 'utc')
-print(tz)
 
+# tests:
 # Сейчас утро
 # 2022-08-25 09:13:55.921148+04:00
