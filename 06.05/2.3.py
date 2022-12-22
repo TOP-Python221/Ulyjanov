6d7846700ee2e09f7caaf19eb2a9ccc3be183408
UlyanovSergey <108969866+UlyanovSergey@users.noreply.github.com>
2022-12-22 12:20:17 +0300

    задание 2.3

--- 06.05/2.3.py
+++ 06.05/2.3.py
@@ -1,17 +1,16 @@
 s = input('Введите строку: ')
 s1 = input('Введите вторую строку: ')
-l = [' ', ',', '.']
 d = {}
 d2 = {}
 for ch in s.lower():
-    if ch not in l:
+    if ch.isalpha():
         if ch in d:
             d[ch] = d[ch] + 1
         else:
             d[ch] = 1
 
 for ch in s1.lower():
-    if ch not in l:
+    if ch.isalpha():
         if ch in d2:
             d2[ch] = d2[ch] + 1
         else:
