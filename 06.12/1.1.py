5e03397acafeedfe73c668d8cfed987f9784d975
UlyanovSergey <108969866+UlyanovSergey@users.noreply.github.com>
2022-12-23 16:13:41 +0300

    задание 1.1

--- 06.12/1.1.py
+++ 06.12/1.1.py
@@ -1,34 +1,34 @@
 day = int(input('Введите номер дня: '))
-monts = int(input('Введите номер месяца: '))
+month = int(input('Введите номер месяца: '))
 year = int(input('Введите год: '))
 
-def ordinalDate(day,monts,year):
+def ordinalDate(day,month,year):
     summ_day = 0
     flag = True
     l = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    if monts <=0:
+    if month <=0:
         return 'Номер месяца не может быть меньше 0 '
-    elif monts > 12:
+    elif month > 12:
         return 'Номер месяца не может быть больше 12 '
     elif day <=0:
-        return 'Номер для не может быть отрецательным'
+        return 'Номер для не может быть отрицательным'
     elif day > 31:
-        return 'число для не может превышать 31го'
+        return 'число для не может быть больше 31'
     # определение високосного года
     if (year % 4 == 0 or year % 400 == 0) and year % 100 != 0:
         flag = True
         l[1] = 29
     else:
         flag = False
-    if monts == 2 and day > 28 and flag == False:
+    if month == 2 and day > 28 and flag == False:
         return 'В этом году в феврале 28 дней'
-    for i in range(monts - 1):
+    for i in range(month - 1):
         summ_day = summ_day + l[i]
     summ_day += day
     return f'порядковый номер дня {summ_day}'
 
-print(ordinalDate(day, monts, year))
+print(ordinalDate(day, month, year))
 
 
 # вывод
