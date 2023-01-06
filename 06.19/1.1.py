6c97706d7ba5838e3a32a9960f04692a58deb85c
UlyanovSergey <108969866+UlyanovSergey@users.noreply.github.com>
2023-01-06 13:07:59 +0300

    задание 1.1

---
+++ 06.19/1.1.py
@@ -0,0 +1,17 @@
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+GCD = gcd(a, b)
+print(GCD)
+
+# вывод
+#
+# Введите первое число: 5
+# Введите второе число: 15
+# 5
